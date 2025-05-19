e8ae2d0e9b4c97e7eb81141b92ad2083ce96030f
Harish Reddy <113511497+harish981@users.noreply.github.com>
2025-05-19 16:05:12 +0530

    Update DbConfig.js

--- application.code/app-tier/DbConfig.js
+++ application.code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : 'database-1.c380a08uukyc.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'harishwar981',
+    DB_PWD : 'password',
     DB_DATABASE : 'webappdb'
 });
